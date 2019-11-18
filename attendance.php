8f9f350262b1ee6257e067d8b2debdca040e43a0
Krishna Ojha <44628947+coder-KO@users.noreply.github.com>
2019-11-18 14:03:00 +0530

    Add files via upload

--- attendance.php
+++ attendance.php
@@ -12,14 +12,13 @@ include('header.php');
       <div class="row">
         <div class="col-md-9">Attendance List</div>
         <div class="col-md-3" align="right">
+          <button type="button" id="chart_button" class="btn btn-primary btn-sm">Chart</button>
           <button type="button" id="report_button" class="btn btn-danger btn-sm">Report</button>
-          <button type="button" id="add_button" class="btn btn-info btn-sm">Add</button>
         </div>
       </div>
     </div>
   	<div class="card-body">
   		<div class="table-responsive">
-        <span id="message_operation"></span>
         <table class="table table-striped table-bordered" id="attendance_table">
           <thead>
             <tr>
@@ -28,6 +27,7 @@ include('header.php');
               <th>Grade</th>
               <th>Attendance Status</th>
               <th>Attendance Date</th>
+              <th>Teacher</th>
             </tr>
           </thead>
           <tbody>
@@ -42,8 +42,8 @@ include('header.php');
 </body>
 </html>
 
-<script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
-<link rel="stylesheet" href="css/datepicker.css" />
+<script type="text/javascript" src="../js/bootstrap-datepicker.js"></script>
+<link rel="stylesheet" href="../css/datepicker.css" />
 
 <style>
     .datepicker
@@ -51,113 +51,6 @@ include('header.php');
       z-index: 1600 !important; /* has to be larger than 1050 */
     }
 </style>
-
-<?php
-
-$query = "
-SELECT * FROM tbl_grade WHERE grade_id = (SELECT teacher_grade_id FROM tbl_teacher 
-    WHERE teacher_id = '".$_SESSION["teacher_id"]."')
-";
-
-$statement = $connect->prepare($query);
-$statement->execute();
-$result = $statement->fetchAll();
-
-?>
-
-<div class="modal" id="formModal">
-  <div class="modal-dialog">
-    <form method="post" id="attendance_form">
-      <div class="modal-content">
-
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title" id="modal_title"></h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-
-        <!-- Modal body -->
-        <div class="modal-body">
-          <?php
-          foreach($result as $row)
-          {
-          ?>
-          <div class="form-group">
-            <div class="row">
-              <label class="col-md-4 text-right">Grade <span class="text-danger">*</span></label>
-              <div class="col-md-8">
-                <?php
-                echo '<label>'.$row["grade_name"].'</label>';
-                ?>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="row">
-              <label class="col-md-4 text-right">Attendance Date <span class="text-danger">*</span></label>
-              <div class="col-md-8">
-                <input type="text" name="attendance_date" id="attendance_date" class="form-control" readonly />
-                <span id="error_attendance_date" class="text-danger"></span>
-              </div>
-            </div>
-          </div>
-          <div class="form-group" id="student_details">
-            <div class="table-responsive">
-              <table class="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th>Roll No.</th>
-                    <th>Student Name</th>
-                    <th>Present</th>
-                    <th>Absent</th>
-                  </tr>
-                </thead>
-                <?php
-                $sub_query = "
-                  SELECT * FROM tbl_student 
-                  WHERE student_grade_id = '".$row["grade_id"]."'
-                ";
-                $statement = $connect->prepare($sub_query);
-                $statement->execute();
-                $student_result = $statement->fetchAll();
-                foreach($student_result as $student)
-                {
-                ?>
-                  <tr>
-                    <td><?php echo $student["student_roll_number"]; ?></td>
-                    <td>
-                      <?php echo $student["student_name"]; ?>
-                      <input type="hidden" name="student_id[]" value="<?php echo $student["student_id"]; ?>" />
-                    </td>
-                    <td>
-                      <input type="radio" name="attendance_status<?php echo $student["student_id"]; ?>" value="Present" />
-                    </td>
-                    <td>
-                      <input type="radio" name="attendance_status<?php echo $student["student_id"]; ?>" checked value="Absent" />
-                    </td>
-                  </tr>
-                <?php
-                }
-                ?>
-              </table>
-            </div>
-          </div>
-          <?php
-          }
-          ?>
-        </div>
-
-        <!-- Modal footer -->
-        <div class="modal-footer">
-          <input type="hidden" name="action" id="action" value="Add" />
-          <input type="submit" name="button_action" id="button_action" class="btn btn-success btn-sm" value="Add" />
-          <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
-        </div>
-
-      </div>
-    </form>
-  </div>
-</div>
 
 <div class="modal" id="reportModal">
   <div class="modal-dialog">
@@ -171,6 +64,15 @@ $result = $statement->fetchAll();
 
       <!-- Modal body -->
       <div class="modal-body">
+        <div class="form-group">
+          <select name="grade_id" id="grade_id" class="form-control">
+            <option value="">Select Grade</option>
+            <?php
+            echo load_grade_list($connect);
+            ?>
+          </select>
+          <span id="error_grade_id" class="text-danger"></span>
+        </div>
         <div class="form-group">
           <div class="input-daterange">
             <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" readonly />
@@ -191,6 +93,44 @@ $result = $statement->fetchAll();
   </div>
 </div>
 
+<div class="modal" id="chartModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Create Grade Attandance Chart</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        <div class="form-group">
+          <select name="chart_grade_id" id="chart_grade_id" class="form-control">
+            <option value="">Select Grade</option>
+            <?php
+            echo load_grade_list($connect);
+            ?>
+          </select>
+          <span id="error_chart_grade_id" class="text-danger"></span>
+        </div>
+        <div class="form-group">
+          <div class="input-daterange">
+            <input type="text" name="attendance_date" id="attendance_date" class="form-control" placeholder="Select Date" readonly />
+            <span id="error_attendance_date" class="text-danger"></span>
+          </div>
+        </div>
+      </div>
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" name="create_chart" id="create_chart" class="btn btn-success btn-sm">Create Chart</button>
+        <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
 <script>
 $(document).ready(function(){
 	
@@ -200,70 +140,15 @@ $(document).ready(function(){
     "order":[],
     "ajax":{
       url:"attendance_action.php",
-      method:"POST",
-      data:{action:"fetch"}
+      type:"POST",
+      data:{action:'fetch'}
     }
   });
 
-  $('#attendance_date').datepicker({
-    format:'yyyy-mm-dd',
-    autoclose:true,
-    container: '#formModal modal-body'
-  });
-
-  function clear_field()
-  {
-    $('#attendance_form')[0].reset();
-    $('#error_attendance_date').text('');
-  }
-
-  $('#add_button').click(function(){
-    $('#modal_title').text("Add Attendance");
-    $('#formModal').modal('show');
-    clear_field();
-  });
-
-  $('#attendance_form').on('submit', function(event){
-    event.preventDefault();
-    $.ajax({
-      url:"attendance_action.php",
-      method:"POST",
-      data:$(this).serialize(),
-      dataType:"json",
-      beforeSend:function(){
-        $('#button_action').val('Validate...');
-        $('#button_action').attr('disabled', 'disabled');
-      },
-      success:function(data)
-      {
-        $('#button_action').attr('disabled', false);
-        $('#button_action').val($('#action').val());
-        if(data.success)
-        {
-          $('#message_operation').html('<div class="alert alert-success">'+data.success+'</div>');
-          clear_field();
-          $('#formModal').modal('hide');
-          dataTable.ajax.reload();
-        }
-        if(data.error)
-        {
-          if(data.error_attendance_date != '')
-          {
-            $('#error_attendance_date').text(data.error_attendance_date);
-          }
-          else
-          {
-            $('#error_attendance_date').text('');
-          }
-        }
-      }
-    })
-  });
-
   $('.input-daterange').datepicker({
-    todayBtn:"linked",
-    format:"yyyy-mm-dd",
-    autoclose:true,
+    todayBtn: "linked",
+    format: "yyyy-mm-dd",
+    autoclose: true,
     container: '#formModal modal-body'
   });
 
@@ -272,9 +157,21 @@ $(document).ready(function(){
   });
 
   $('#create_report').click(function(){
+    var grade_id = $('#grade_id').val();
     var from_date = $('#from_date').val();
     var to_date = $('#to_date').val();
     var error = 0;
+
+    if(grade_id == '')
+    {
+      $('#error_grade_id').text('Grade is Required');
+      error++;
+    }
+    else
+    {
+      $('#error_grade_id').text('');
+    }
+
     if(from_date == '')
     {
       $('#error_from_date').text('From Date is Required');
@@ -300,7 +197,46 @@ $(document).ready(function(){
       $('#from_date').val('');
       $('#to_date').val('');
       $('#formModal').modal('hide');
-      window.open("report.php?action=attendance_report&from_date="+from_date+"&to_date="+to_date);
+      window.open("report.php?action=attendance_report&grade_id="+grade_id+"&from_date="+from_date+"&to_date="+to_date);
+    }
+
+  });
+
+  $('#chart_button').click(function(){
+    $('#chart_grade_id').val('');
+    $('#attendance_date').val('');
+    $('#chartModal').modal('show');
+  });
+
+  $('#create_chart').click(function(){
+    var grade_id = $('#chart_grade_id').val();
+    var attendance_date = $('#attendance_date').val();
+    var error = 0;
+    if(grade_id == '')
+    {
+      $('#error_chart_grade_id').text('Grade is Required');
+      error++;
+    }
+    else
+    {
+      $('#error_chart_grade_id').text('');
+    }
+    if(attendance_date == '')
+    {
+      $('#error_attendance_date').text('Date is Required');
+      $error++;
+    }
+    else
+    {
+      $('#error_attendance_date').text('');
+    }
+
+    if(error == 0)
+    {
+      $('#attendance_date').val('');
+      $('#chart_grade_id').val('');
+      $('#chartModal').modal('show');
+      window.open("chart1.php?action=attendance_report&grade_id="+grade_id+"&date="+attendance_date);
     }
 
   });
